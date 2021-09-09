297eca7f565ba0e8d92c121092e5c30fc444b8f6
Phorutai <37576892+Phorutai@users.noreply.github.com>
2021-09-10 00:04:53 +0100

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 import setuptools
 
 setup(
-    name="MHCVision",
+    name="MHCVision-RF",
     version="1.0",
-    description="FDR/PEP estimation for MHC-peptide binding prediction",
     author='Phorutai Pearngam',
     author_email='p.pearngam@liverpool.ac.uk',
-    url="https://github.com/PGB-LIV/MHCVision/",
+    url="https://github.com/PGB-LIV/MHCVision-RF/",
     packages=setuptools.find_packages(),
     zip_safe=False,
     classifiers="Programming Language :: Python :: 3.7",
