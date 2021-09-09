1c41a7a0961adab15899b746e8dab87a2da75719
Phorutai <37576892+Phorutai@users.noreply.github.com>
2021-09-10 00:03:52 +0100

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name="MHCVision",
+    version="1.0",
+    description="FDR/PEP estimation for MHC-peptide binding prediction",
+    author='Phorutai Pearngam',
+    author_email='p.pearngam@liverpool.ac.uk',
+    url="https://github.com/PGB-LIV/MHCVision/",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers="Programming Language :: Python :: 3.7",
+
+)
